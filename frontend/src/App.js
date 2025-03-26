d67ea01adf8755898c8bcfe197cca05baf75317e
marion586 <marionmenye@gmail.com>
2025-03-26 08:29:46 +0300

    created authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { LoginPage } from "./Routes";
+import { LoginPage, SignUpPage } from "./Routes";
 import "./App.css";
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />,
+        <Route path="/sign-up" element={<SignUpPage />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,3 +1,3 @@
-import LoginPage from "../src/pages/Login.jsx";
-
-export { LoginPage };
+import LoginPage from "./pages/LoginPage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+export { LoginPage, SignUpPage };
