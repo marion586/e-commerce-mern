247661bb945c4e144c458725eee36e729bb3b43c
marion586 <marionmenye@gmail.com>
2025-04-07 07:33:44 +0300

    create Hero component

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import Header from "../components/Layout/Header.jsx";
+import Hero from "../components/Route/Hero/Hero.jsx";
+
 const HomePage = () => {
   const [activeHeading , setactiveHeading] = useState(1)
   return (
     <div>
       <Header activeHeading={activeHeading} />
+      <Hero />
     </div>
   );
 };
