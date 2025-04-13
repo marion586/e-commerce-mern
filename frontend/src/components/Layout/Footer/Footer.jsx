4f638f0bd177dfec0740499a25695b57072e2e95
marion586 <marionmenye@gmail.com>
2025-04-13 16:59:48 +0300

    created footer and sponsored

---
+++ frontend/src/components/Layout/Footer/Footer.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-[#000] text-white">
+      <div className="md:flex md:justify-between md:items-start sm:px-12 px-4 bg-[#342ac8] py-7">
+        <h1 className="lg:text-4xl text-3xl md:mb-0 mb-6 lg:leading-normal font-semibold md:w-2/5">
+          <span className="text-[#56d879]">Subscribe</span> us for get new{" "}
+          <br />
+        </h1>
+        <div>
+          <input
+            type="text"
+            required
+            placeholder="Enter you email..."
+            className="text-gray-100 sm:w-72  w-full sm:mr-5 mr-1 lg:mb-0 mb-4 py-2.5 rounded px-2 focus:outline-none"
+          />
+
+          <button className="bg-[#56d879] hover:bg-teal-500 duration-300 px-5 py-2.5 rounded-md text-white md:w-auto w-full">
+            Submit
+          </button>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1  sm:grid-cols-3 lg:grid-cols-4 gap-6 sm:px-5 py-16 sm:text-center">
+        <ul className="px-5 text-center sm:text-start flex sm:block flex-col items-center">
+          <img
+            src="https://shopo.quomodothemes.website/assets/images/logo.svg"
+            alt=""
+            style={{ filter: "brightness(0) invert(1)" }}
+          />
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,6 +5,8 @@ import Categories from "../components/Route/Categories/Categories.jsx";
 import BestDeals from "../components/Route/BestDeals/BestDeals.jsx";
 import FeaturedProduct from "../components/Route/FeaturedProduct/FeaturedProduct.jsx";
 import Events from "../components/Route/Events/Events.jsx";
+import Sponsored from "../components/Route/Sponsored/Sponsored.jsx";
+import Footer from "../components/Layout/Footer/Footer.jsx";
 
 const HomePage = () => {
   const [activeHeading, setactiveHeading] = useState(1);
@@ -17,6 +19,10 @@ const HomePage = () => {
       <FeaturedProduct />
 
       <Events />
+
+      <Sponsored />
+
+      <Footer />
     </div>
   );
 };
