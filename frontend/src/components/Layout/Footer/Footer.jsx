8d9a30794cbd50bb02de7a52e2630548c1f9b3f7
marion586 <marionmenye@gmail.com>
2025-04-13 17:23:15 +0300

    add footer

--- frontend/src/components/Layout/Footer/Footer.jsx
+++ frontend/src/components/Layout/Footer/Footer.jsx
@@ -1,5 +1,16 @@
 import React from "react";
-
+import {
+  AiFillFacebook,
+  AiFillInstagram,
+  AiFillYoutube,
+  AiOutlineTwitter,
+} from "react-icons/ai";
+import { Link } from "react-router-dom";
+import {
+  footercompanyLinks,
+  footerProductLinks,
+  footerSupportLinks,
+} from "../../../static/data";
 const Footer = () => {
   return (
     <div className="bg-[#000] text-white">
@@ -29,7 +40,73 @@ const Footer = () => {
             alt=""
             style={{ filter: "brightness(0) invert(1)" }}
           />
+          <br />
+
+          <p>The home and elements needeed to create beautifull products</p>
+          <div className="flex items-center mt-[15px]">
+            <AiFillFacebook size={25} className=" ml-[15px] cursor-pointer" />
+
+            <AiOutlineTwitter size={25} className="ml-[15px] cursor-pointer" />
+            <AiFillInstagram size={25} className=" ml-[15px] cursor-pointer" />
+            <AiFillYoutube size={25} className=" ml-[15px] cursor-pointer" />
+          </div>
         </ul>
+
+        <ul className="text-center sm:text-start">
+          <h1 className="mb-1  font-semibold">Company</h1>
+
+          {footerProductLinks.map((link) => (
+            <li key={link.name}>
+              <Link
+                className="text-gray-100 hover:text-teal-400 duration-300 text-sm cursor-pointer leading-6"
+                to={link.link}
+              >
+                {link.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+
+        <ul className="text-center sm:text-start">
+          <h1 className="mb-1  font-semibold">Shop</h1>
+
+          {footercompanyLinks.map((link) => (
+            <li key={link.name}>
+              <Link
+                className="text-gray-100 hover:text-teal-400 duration-300 text-sm cursor-pointer leading-6"
+                to={link.link}
+              >
+                {link.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+
+        <ul className="text-center sm:text-start">
+          <h1 className="mb-1  font-semibold">Support</h1>
+
+          {footerSupportLinks.map((link) => (
+            <li key={link.name}>
+              <Link
+                className="text-gray-100 hover:text-teal-400 duration-300 text-sm cursor-pointer leading-6"
+                to={link.link}
+              >
+                {link.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 text-center pt-2 text-gray-400 text-sm pb-8">
+        <span>2025 Marion . All rights reserve, </span>
+        <span>Terms . Privacy Policy</span>
+        <div className="sm:block flex items-center justify-center w-full">
+          <img
+            src="https://hamart-shop.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ffooter-payment.a37c49ac.png&w=640&q=75"
+            alt=""
+          />
+        </div>
       </div>
     </div>
   );
